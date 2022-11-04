246ef76c1726ca4cdcc9762da7776b8b0963022d
q-interview <117466502+q-interview@users.noreply.github.com>
2022-11-05 00:39:37 +0100

    Improve: Remove section wrapper (#8)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as CustomCheck } from "./CustomCheck";
 export { default as Header } from "./Header";
 export { default as Message } from "./Message";
-export { default as SectionWrapper } from "./SectionWrapper";
 export { default as ThemeProvider } from "./ThemeProvider";
 export { default as UserFilter } from "./UserFilter";
 export { default as UserInfo } from "./UserInfo";
